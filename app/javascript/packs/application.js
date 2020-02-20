6686abceb8d0a57b4e126a77614ad8822dff092f
Rudi Boeke <r.boeke@me.com>
2020-02-20 11:50:38 +0100

    setup mister cocktail

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 /* eslint no-console:0 */
+import 'bootstrap';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -14,5 +15,7 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
 
 console.log('Hello World from Webpacker')
